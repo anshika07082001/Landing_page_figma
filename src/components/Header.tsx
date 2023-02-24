ebd1590b6f5450dcbb060d2a20e352593330afde
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2023-02-24 19:00:41 +0530

    final

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='banner margin pr'>
+        <div className='banner__ellipse'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
